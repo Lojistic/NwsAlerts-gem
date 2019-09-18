6b7da4ff4865d2578a7dc91a9032aec15ae4fa52
Steve Lewis <steve.l@lojistic.com>
2019-09-18 16:29:39 -0700

    More filter options

--- spec/nws/api/alerts/client_spec.rb
+++ spec/nws/api/alerts/client_spec.rb
@@ -15,11 +15,23 @@ RSpec.describe Nws::Api::Alerts::Client do
 
   describe "#fetch_raw_alerts" do
     it "should return parsed JSON" do
-      client = Nws::Api::Alerts::Client.new
+      expect(Nws::Api::Alerts::Client).to receive(:get).and_return("{}")
+
+      client     = Nws::Api::Alerts::Client.new
       raw_alerts = client.fetch_raw_alerts
 
       expect(raw_alerts).to be_a Hash
     end
+
+    context "when given parameters for the API call" do
+      it "should add the parameters to the endpoint URL" do
+        expect(Nws::Api::Alerts::Client).to receive(:get).with("/alerts?message_type=alert&status=actual&urgency=future&severity=severe").and_return("{}")
+        client = Nws::Api::Alerts::Client.new
+        client.fetch_raw_alerts(severity: 'severe', urgency: 'future')
+      end
+    end
+
   end
+
 
 end
